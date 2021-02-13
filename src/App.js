d4a8e663cf626c01a6c93a6d7bc3fd383cdc7dfb
Hanh Solo <minhhanhnyc@gmail.com>
2021-02-13 10:29:59 -0800

    set env conditiontal statment, import components to app.js and updated ci.yml

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import LocationSearchForm from './components/LocationSearchForm';
+
+let BASE_URL =''
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  BASE_URL ="http://localhost:5000/"
+} else {
+  BASE_URL= "https://hidden-escarpment-16617.herokuapp.com/"
+}
 
 
 function App() {
+
+  const [location, setLocation] = useState(null);
+  const onLocationPicked = (currentCity) => {
+    setLocation(currentCity)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       <h3> This is testing deployment </h3>
+      <div className='weather-box'>
+          <div className='temp'>
+            <LocationSearchForm searchCurrentCityCallBack={ onLocationPicked } />
+            <Navigation />
+            <Main location={ location } baseUrl={ BASE_URL } />
+          </div>
+        </div>
       </header>
     </div>
   );
