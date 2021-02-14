5b3c945d5661900649a81b733b603ef092a09497
Hanh Solo <minhhanhnyc@gmail.com>
2021-02-14 11:31:00 -0800

    updated file dir and install weather icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './styles/App.css';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
 import LocationSearchForm from './components/LocationSearchForm';
