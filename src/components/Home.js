e23996ab4ea7b2035c4a00cec29b8a9894b31dee
Hanh Solo <minhhanhnyc@gmail.com>
2021-02-16 17:04:59 -0800

    Code Freeze

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 const Home = () => (
   <div className='container'>
     <div className='home-page app'>
-      <h4>Your home to the most trusted weather temperature and forecast</h4>
+      <h3>Your home to the most trusted weather temperature and forecast</h3>
     </div>
   </div>
   
