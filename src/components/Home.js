edca28d38a57fcdc37da1bc8818b6066cfcf9529
Hanh Solo <minhhanhnyc@gmail.com>
2021-02-18 15:34:30 -0800

    testing redirect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import './Home.css';
 
+const [redirect, setRedirect] = useState(false);
 const Home = () => (
   <div className='container'>
+    <Redirect to={{pathname: "/temperature", state: { from: props.location } }}/>
     <div className='home-page app'>
       <h3>Your home to the most trusted weather temperature and forecast</h3>
     </div>
